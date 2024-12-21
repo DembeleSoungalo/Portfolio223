79ce4d59d62f05021d710cb9b55e43f70f10fba0
DembeleSoungalo <dembelesalam27@gmail.com>
2024-12-21 17:06:53 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,49 +1,32 @@
-import React, { useState, useEffect } from "react";
-import Preloader from "../src/components/Pre";
-import Navbar from "./components/Navbar";
-import Home from "./components/Home/Home";
-import About from "./components/About/About";
-import Projects from "./components/Projects/Projects";
-import Footer from "./components/Footer";
-import Resume from "./components/Resume/ResumeNew";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate
-} from "react-router-dom";
-import ScrollToTop from "./components/ScrollToTop";
-import "./style.css";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React from 'react';
+import MainProvider from './State/MainContext';
+import InfoSection from './Components/Project/InfoSection';
+import SearchSection from './Components/Project/SearchSection';
+import './App.css'
+import  'bootstrap/dist/css/bootstrap.min.css';
+
+import {Navbar,Container} from 'react-bootstrap';
+
+
 
 function App() {
-  const [load, upadateLoad] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      upadateLoad(false);
-    }, 1200);
-
-    return () => clearTimeout(timer);
-  }, []);
-
+  
   return (
-    <Router>
-      <Preloader load={load} />
-      <div className="App" id={load ? "no-scroll" : "scroll"}>
-        <Navbar />
-        <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/project" element={<Projects />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/resume" element={<Resume />} />
-          <Route path="*" element={<Navigate to="/"/>} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+    <div className="App">
+
+      <Navbar expand="lg" bg='dark' variant='dark' >
+        <Container>
+          <Navbar.Brand >Github User </Navbar.Brand>
+        </Container>
+      </Navbar>
+    
+   <MainProvider>
+   <SearchSection/>
+
+    <InfoSection></InfoSection>
+   </MainProvider>
+    </div>
+    
   );
 }
 
